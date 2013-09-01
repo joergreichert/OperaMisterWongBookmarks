91fe489be08d3483af5c9664724fbff6fda6b74d
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2013-09-01 15:59:06 +0200

    Fixed Guice exception with help of http://www.eclipse.org/forums/index.php/t/472169/

--- de.abg.jreichert.forms.ui/src/de/abg/jreichert/forms/ui/Activator.java
+++ de.abg.jreichert.forms.ui/src/de/abg/jreichert/forms/ui/Activator.java
@@ -1,12 +1,18 @@
 package de.abg.jreichert.forms.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.resource.IStorage2UriMapperJdtExtensions;
+import org.eclipse.xtext.ui.resource.Storage2UriMapperJavaImpl;
+import org.eclipse.xtext.ui.shared.JdtHelper;
 import org.eclipse.xtext.ui.shared.internal.SharedModule;
+import org.eclipse.xtext.ui.util.IJdtHelper;
 import org.eclipse.xtext.util.Modules2;
 import org.osgi.framework.BundleContext;
 
+import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Module;
 
 import de.abg.jreichert.MisterWongDslRuntimeModule;
 import de.abg.jreichert.OperaDSLRuntimeModule;
@@ -24,9 +30,9 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	private Injector operaInjector, misterWongInjector;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -35,18 +41,30 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		operaInjector = Guice.createInjector(Modules2.mixin(new SharedModule(), new OperaDSLRuntimeModule(), new OperaDSLUiModule(this), new BookmarkFormsModule()));
-		misterWongInjector = Guice.createInjector(Modules2.mixin(new SharedModule(), new MisterWongDslRuntimeModule(), new MisterWongDslUiModule(this), new BookmarkFormsModule()));
+		operaInjector = Guice.createInjector(Modules2.mixin(new SharedModule(),
+				new IStorage2UriMapperAwareModule(),
+				new OperaDSLRuntimeModule(), new OperaDSLUiModule(this),
+				new BookmarkFormsModule()));
+		misterWongInjector = Guice.createInjector(Modules2.mixin(
+				new SharedModule(), new IStorage2UriMapperAwareModule(),
+				new MisterWongDslRuntimeModule(), new MisterWongDslUiModule(
+						this), new BookmarkFormsModule()));
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -55,7 +73,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -65,8 +83,18 @@ public class Activator extends AbstractUIPlugin {
 	public Injector getOperaInjector() {
 		return operaInjector;
 	}
-	
+
 	public Injector getMisterWongInjector() {
 		return misterWongInjector;
+	}
+
+	class IStorage2UriMapperAwareModule implements Module {
+
+		@Override
+		public void configure(Binder binder) {
+			binder.bind(IStorage2UriMapperJdtExtensions.class).to(
+					Storage2UriMapperJavaImpl.class);
+			binder.bind(IJdtHelper.class).to(JdtHelper.class);
+		}
 	}
 }
