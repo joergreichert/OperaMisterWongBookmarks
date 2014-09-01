866043c038289a7227a954de3a1fede91727a82c
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2014-09-01 11:57:36 +0200

    Xtext 2.7. migration in progress

--- de.abg.jreichert.wongdsl.tests/src/de/abg/jreichert/formatting/FormatterTest.java
+++ de.abg.jreichert.wongdsl.tests/src/de/abg/jreichert/formatting/FormatterTest.java
@@ -1,58 +1,76 @@
 package de.abg.jreichert.formatting;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 import org.eclipse.xtext.formatting.INodeModelFormatter;
 import org.eclipse.xtext.formatting.INodeModelFormatter.IFormattedRegion;
-import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipse.xtext.junit4.parameterized.InjectParameter;
-import org.eclipse.xtext.junit4.parameterized.ParameterizedXtextRunner;
-import org.eclipse.xtext.junit4.parameterized.ResourceURIs;
-import org.eclipse.xtext.junit4.validation.AbstractValidatorTester;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.resource.XtextResource;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.xpect.expectation.IStringExpectation;
+import org.xpect.expectation.StringExpectation;
+import org.xpect.parameter.ParameterParser;
+import org.xpect.runner.Xpect;
+import org.xpect.runner.XpectRunner;
+import org.xpect.runner.XpectTestFiles;
+import org.xpect.runner.XpectTestFiles.FileRoot;
+import org.xpect.setup.XpectSetup;
+import org.xpect.xtext.lib.setup.ThisOffset;
+import org.xpect.xtext.lib.setup.ThisResource;
+import org.xpect.xtext.lib.setup.XtextStandaloneSetup;
+import org.xpect.xtext.lib.setup.XtextValidatingSetup;
 
 import com.google.inject.Inject;
 
-import de.abg.jreichert.MisterWongDslInjectorProvider;
+import de.abg.jreichert.formatting.FormatterTest.NullValidatorSetup;
 
+@RunWith(XpectRunner.class)
+@XpectTestFiles(relativeTo = FileRoot.PROJECT, baseDir = "res/formatter/unformatted", fileExtensions = "wong")
+@XpectSetup({ XtextStandaloneSetup.class, NullValidatorSetup.class })
+public class FormatterTest {
+	private static String UNIX_LINE_ENDING = "\n";
+	private static String WINDOWS_LINE_ENDING = "\r\n";
+	private static String LINE_SEPARATOR = System.getProperty("line.separator");
 
-@RunWith(ParameterizedXtextRunner.class)
-@InjectWith(MisterWongDslInjectorProvider.class)
-@ResourceURIs(baseDir = "res/formatter/unformatted", fileExtensions = "wong")
-public class FormatterTest extends AbstractValidatorTester {
- 
-	@InjectParameter
-	protected XtextResource resource;
- 
+	public static class NullValidatorSetup extends XtextValidatingSetup {
+		public NullValidatorSetup(@ThisResource XtextResource resource) {
+			super(resource);
+		}
+
+		@Override
+		public void validate() {}
+	}
+
 	@Inject
-	protected INodeModelFormatter formatter;
- 
-	public String formatted() {
+	private INodeModelFormatter formatter;
+
+	@ParameterParser(syntax = "('from' offset=OFFSET 'to' to=OFFSET)?")
+	@Xpect
+	public void formatted(@StringExpectation(whitespaceSensitive = true) IStringExpectation expectation,
+			@ThisResource XtextResource resource, @ThisOffset int offset, @ThisOffset int to) {
 		ICompositeNode rootNode = resource.getParseResult().getRootNode();
-		IFormattedRegion r = formatter.format(rootNode, 0, rootNode.getLength());
-		return r.getFormattedText();
-	}
-	
-	@Ignore
-	@Test
-	public void testFormatter() throws Exception {
-//		String expectedPath = "res/formatter/formatted/mdsd.wong"; 
-//		String expected = getFileContent(expectedPath);
-//		String formatted = formatted();
-//		Assert.assertEquals(expected, formatted);
+		IFormattedRegion r = null;
+		if (offset >= 0 && to > offset) {
+			r = formatter.format(rootNode, offset, to - offset);
+		} else {
+			r = formatter.format(rootNode, rootNode.getOffset(), rootNode.getTotalLength());
+		}
+		String formatted = r.getFormattedText();
+		if (isUnixLineEnding()) {
+			formatted = formatted.replaceAll(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING);
+		} else if (isWindowsLineEnding()) {
+			if (!rootNode.getText().contains(WINDOWS_LINE_ENDING)) {
+				formatted = formatted.replaceAll(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING);
+			} else {
+				formatted = formatted.replaceAll("(!\r)\n", WINDOWS_LINE_ENDING);
+			}
+		}
+		expectation.assertEquals(formatted);
 	}
 
-	private String getFileContent(String path) throws FileNotFoundException {
-		Scanner scanner = new Scanner(new FileInputStream(new File(path)), "UTF-8");
-		String given = scanner.useDelimiter("\\Z").next();
-		scanner.close();
-		return given;
+	private static boolean isWindowsLineEnding() {
+		return WINDOWS_LINE_ENDING.equals(LINE_SEPARATOR);
+	}
+
+	private static boolean isUnixLineEnding() {
+		return UNIX_LINE_ENDING.equals(LINE_SEPARATOR);
 	}
 }
