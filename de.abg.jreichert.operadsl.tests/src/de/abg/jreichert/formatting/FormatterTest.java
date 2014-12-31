449c296389985c7b17d826f76b41c1d5e8f37ea3
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2014-12-31 10:10:05 +0100

    switched to non-Xpect based formatter test, as DSLs don't have a comment rule

--- de.abg.jreichert.operadsl.tests/src/de/abg/jreichert/formatting/FormatterTest.java
+++ de.abg.jreichert.operadsl.tests/src/de/abg/jreichert/formatting/FormatterTest.java
@@ -1,44 +1,69 @@
 package de.abg.jreichert.formatting;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import org.eclipse.xtext.formatting.INodeModelFormatter;
 import org.eclipse.xtext.formatting.INodeModelFormatter.IFormattedRegion;
+import org.eclipse.xtext.junit4.AbstractXtextTests;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
-import org.eclipse.xtext.resource.XtextResource;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.xpect.expectation.IStringExpectation;
-import org.xpect.expectation.StringExpectation;
-import org.xpect.parameter.ParameterParser;
-import org.xpect.runner.Xpect;
-import org.xpect.runner.XpectRunner;
-import org.xpect.runner.XpectTestFiles;
-import org.xpect.runner.XpectTestFiles.FileRoot;
-import org.xpect.setup.XpectSetup;
-import org.xpect.xtext.lib.setup.ThisOffset;
-import org.xpect.xtext.lib.setup.ThisResource;
-import org.xpect.xtext.lib.setup.XtextStandaloneSetup;
 
+import com.google.common.io.Closer;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 
-@RunWith(XpectRunner.class)
-@XpectTestFiles(relativeTo = FileRoot.PROJECT, baseDir = "res/formatter", fileExtensions = "adr")
-@XpectSetup(XtextStandaloneSetup.class)
-public class FormatterTest {
+import de.abg.jreichert.OperaDSLInjectorProvider;
+
+@RunWith(XtextRunner.class)
+@InjectWith(OperaDSLInjectorProvider.class)
+public class FormatterTest extends AbstractXtextTests {
+    @Inject
+    Injector injector;;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        setInjector(injector);
+    }
 
     @Inject
     protected INodeModelFormatter formatter;
 
-    @ParameterParser(syntax = "('from' offset=OFFSET 'to' to=OFFSET)?")
-    @Xpect
-    public void formatted(@StringExpectation(whitespaceSensitive = true) IStringExpectation expectation,
-            @ThisResource XtextResource resource, @ThisOffset int offset, @ThisOffset int to) {
-        ICompositeNode rootNode = resource.getParseResult().getRootNode();
-        IFormattedRegion r = null;
-        if (offset >= 0 && to > offset) {
-            r = formatter.format(rootNode, offset, to - offset);
-        } else {
-            r = formatter.format(rootNode, rootNode.getOffset(), rootNode.getTotalLength());
+    public String formatted(String path) throws Exception {
+        Closer closer = Closer.create();
+        try {
+            FileInputStream fis = closer.register(new FileInputStream(new File(path)));
+            ICompositeNode rootNode = super.getRootNode(fis);
+            IFormattedRegion r = formatter.format(rootNode, 0, rootNode.getLength());
+            return r.getFormattedText();
+        } finally {
+            closer.close();
         }
-        String formatted = r.getFormattedText();
-        expectation.assertEquals(formatted);
+    }
+
+    @Test
+    @Ignore("not finished yet.")
+    public void testFormatter() throws Exception {
+        String expectedPath = "res/formatter/formatted/mdsd.adr";
+        String expected = getFileContent(expectedPath);
+        String formatted = formatted("res/formatter/unformatted/mdsd.adr");
+        Assert.assertEquals(expected, formatted);
+    }
+
+    private String getFileContent(String path) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileInputStream(new File(path)), "UTF-8");
+        String given = scanner.useDelimiter("\\Z").next();
+        scanner.close();
+        return given;
     }
 }
